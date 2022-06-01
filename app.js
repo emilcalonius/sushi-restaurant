f9209cd1520e06e9e2afe25298e7a11284edf814
Emil Calonius <emil.calonius@tuni.fi>
2022-06-01 20:38:22 +0300

    Animate menu opening and closing

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+// Handles opening and closing of menu by clicking hamburger icon
 function openMenu() {
   // Toggle between hamburger menu or x icons
   document.getElementById("menu-button").classList.toggle("change");
 
   // Hide or show menu
   const menu = document.getElementById("menu");
-  menu.style.display === "none" ? menu.style.display = "block" : menu.style.display = "none";
+  menu.dataset.state == "open" ? menu.dataset.state = "closed" : menu.dataset.state = "open";
 }
